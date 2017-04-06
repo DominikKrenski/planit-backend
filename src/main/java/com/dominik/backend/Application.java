1eee4cba9642588aec16b5a64a21368180a6f03d
Dominik Kreński <dominik.krenski@gmail.com>
2017-04-06 10:10:58 +0200

    Dodano klasę uruchomieniową aplikacji

---
+++ src/main/java/com/dominik/backend/Application.java
@@ -0,0 +1,16 @@
+package com.dominik.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dominik on 06.04.17.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
