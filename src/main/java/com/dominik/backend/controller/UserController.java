5e050e0c8f71f0de01ee292f9d679f8af1d0e23d
Dominik Kreński <dominik.krenski@gmail.com>
2017-04-07 09:19:29 +0200

    Dodano adnotacje SWAGGER do klasy UserController

--- src/main/java/com/dominik/backend/controller/UserController.java
+++ src/main/java/com/dominik/backend/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.dominik.backend.controller;
 
+import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/user")
+@Api(tags = UserController.URL, description = "Endpoint for user management")
 public class UserController {
+
+    public static final String URL = "planit-backend.com:8080/api";
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
+    
 }
