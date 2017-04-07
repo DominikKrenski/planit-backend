ae4515cecf7edc49ce51447c4c2358fbe3f8192e
Dominik Kreński <dominik.krenski@gmail.com>
2017-04-07 09:16:08 +0200

    Dodano klasę UserController odpowiedzialną za obsługę żądań związanych z użytkownikami aplikacji

---
+++ src/main/java/com/dominik/backend/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.dominik.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by dominik on 07.04.17.
+ */
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+}
