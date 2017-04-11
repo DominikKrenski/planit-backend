e07f07bd5252208e002b0fcf227234b08f1c50d2
Dominik Kreński <dominik@MacBook-Air-Dominik.local>
2017-04-11 17:34:10 +0200

    Dodano klasę AccountCredentials, która ma za zadanie przechowywać login oraz hasło

---
+++ src/main/java/com/dominik/backend/security/AccountCredentials.java
@@ -0,0 +1,32 @@
+package com.dominik.backend.security;
+
+/**
+ * Created by dominik on 11.04.2017.
+ */
+
+public class AccountCredentials {
+
+    private String login;
+    private String password;
+
+    public AccountCredentials(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
