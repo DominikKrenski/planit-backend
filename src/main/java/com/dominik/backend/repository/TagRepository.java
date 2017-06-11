a7d8dfdc86d29395087c338a06b4e5cfe5019742
Dominik Kreński <dominik.krenski@gmail.com>
2017-06-11 20:33:53 +0200

    Utworzono repozytorium TagRepository

---
+++ src/main/java/com/dominik/backend/repository/TagRepository.java
@@ -0,0 +1,10 @@
+package com.dominik.backend.repository;
+
+import com.dominik.backend.entity.Tag;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by dominik on 11.06.2017.
+ */
+public interface TagRepository extends CrudRepository<Tag, Long> {
+}
