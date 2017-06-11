ef4f71f267285c0cff586c80c76979f4538b6438
Dominik Kreński <dominik.krenski@gmail.com>
2017-06-11 20:32:09 +0200

    Dodano adnotacje dla walidacji, Swaggera oraz Jacksona do klasy Tag

--- src/main/java/com/dominik/backend/entity/Tag.java
+++ src/main/java/com/dominik/backend/entity/Tag.java
@@ -1,6 +1,13 @@
 package com.dominik.backend.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 /**
  * Created by dominik on 11.06.2017.
@@ -14,15 +21,24 @@ public class Tag {
     @SequenceGenerator(sequenceName = "tags_id_seq", name = "TagIdSequence", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TagIdSequence")
     @Column(name = "id")
+    @ApiModelProperty(notes = "Tag's id (empty if request incomes)", required = false, position = 1)
     private Long id;
 
     @Column(name = "name", length = 20, nullable = false)
+    @NotNull(message = "{null.message}")
+    @Size(max = 20, message = "{tagNameLength.message}")
+    @Pattern(regexp = "^[A-Za-zĘÓĄŚŁŻŹĆŃęóąśłżźćń]+", message = "{tagNamePattern.message}")
+    @JsonProperty("NAME")
+    @ApiModelProperty(notes = "Name of the tag", required = true, position = 2)
     private String name;
 
     @Column(name = "is_accepted", nullable = false)
+    @NotNull(message = "{null.message}")
+    @JsonProperty("IS_ACCEPTED")
+    @ApiModelProperty(notes = "Status of the tag", required = true, position = 3)
     private boolean isAccepted;
 
-    private Tag() { }
+    protected Tag() { }
 
     public Tag(String name, boolean isAccepted) {
         this.id = 0L;
@@ -30,10 +46,12 @@ public class Tag {
         this.isAccepted = isAccepted;
     }
 
+    @JsonIgnore
     public void setId(Long id) {
         this.id = id;
     }
 
+    @JsonProperty("ID")
     public Long getId() {
         return id;
     }
