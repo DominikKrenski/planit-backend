29962a9b99853d2590c174b1fb879f9188dfb4fc
Dominik Kreński <dominik.krenski@gmail.com>
2017-12-04 06:51:03 +0100

    Dodano metodę do zapisywania powiadomienia

--- src/main/java/com/dominik/backend/service/NotificationService.java
+++ src/main/java/com/dominik/backend/service/NotificationService.java
@@ -1,4 +1,8 @@
 package com.dominik.backend.service;
 
+import com.dominik.backend.entity.Notification;
+
 public interface NotificationService {
+
+    Notification saveNotification(Notification notification);
 }
--- src/main/java/com/dominik/backend/service/NotificationServiceImpl.java
+++ src/main/java/com/dominik/backend/service/NotificationServiceImpl.java
@@ -1,4 +1,20 @@
 package com.dominik.backend.service;
 
+import com.dominik.backend.entity.Notification;
+import com.dominik.backend.repository.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class NotificationServiceImpl implements NotificationService {
+
+    private NotificationRepository notificationRepository;
+
+    @Autowired
+    public NotificationServiceImpl(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
+    @Override
+    public Notification saveNotification(Notification notification) {
+        return notificationRepository.save(notification);
+    }
 }
